0653a115cf41820d4d5a8f8afce2015fcb6edea8
TellSamm <chernikov.semen21@gmail.com>
2025-07-17 22:36:34 +0300

    -- удалил лишние поля (эх доработочка version 3)

--- migrations/20250717063043_users.up.sql
+++ migrations/20250717063043_users.up.sql
@@ -1,8 +1,11 @@
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    email TEXT UNIQUE NOT NULL,
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+-- Создаёт расширение pgcrypto, которое нужно для генерации UUID через gen_random_uuid()
+-- Добавлено вручную для поддержки автоматической генерации UUID в PostgreSQL
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL,
     password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
