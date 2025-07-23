6e6c66227852b38418b4fae0fe560ce57516eb96
TellSamm <chernikov.semen21@gmail.com>
2025-07-23 15:34:38 +0300

    -- доработки третьего задания v6

--- tasks-service/cmd/server/main.go
+++ tasks-service/cmd/server/main.go
@@ -9,21 +9,17 @@ import (
 )
 
 func main() {
-	//Инициализация БД
 	database.InitDB()
 
-	//Репозиторий и сервис задач
 	repo := task.NewTaskRepository(database.DB)
 	svc := task.NewTaskService(repo)
 
-	//Клиент к Users-сервису
 	userClient, conn, err := transportgrpc.NewUserClient("localhost:50051")
 	if err != nil {
 		log.Fatalf("failed to connect to users: %v", err)
 	}
 	defer conn.Close()
 
-	//Запуск gRPC Tasks-сервиса
 	if err := transportgrpc.RunGRPC(svc, userClient); err != nil {
 		log.Fatalf("Tasks gRPC server error: %v", err)
 	}
